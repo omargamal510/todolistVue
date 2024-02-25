22194ab0c6be88f9a526d37c5cb70ba941563250
omargamal510 <34138436+omargamal510@users.noreply.github.com>
2024-02-25 21:13:41 +0200

    uploading todoList

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="app">
+		<PageTitle :title="getPageTitle" />
+		<Dashboard />
+		<UIMessage>
+			<p>This application uses the browser's Local Storage to store data.</p>
+		</UIMessage>
+	</div>
+</template>
+
+<script>
+	import { mapGetters } from "vuex";
+	import PageTitle from "@/components/Page/PageTitle.vue";
+	import Dashboard from "@/components/Dashboard/Dashboard.vue";
+	import UIMessage from "@/components/UI/UIMessage.vue";
+
+	export default {
+		"components": {
+			PageTitle,
+			Dashboard,
+			UIMessage,
+		},
+		"computed": {
+			...mapGetters([
+				"getPageTitle",
+			]),
+		},
+	};
+</script>
+
+<style lang="scss">
+	.app {
+		width: 100%;
+	}
+
+	[v-cloak] {
+		display: none;
+	}
+</style>
